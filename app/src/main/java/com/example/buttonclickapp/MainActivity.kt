8850ccceb303188e59c12019a39e049c071a9d1c
Ron Johnson <ronjohn1414@gmail.com>
2019-08-18 23:18:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.buttonclickapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+//    private var userInput: EditText? = null
+//    private var button: Button? = null
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userInput = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                textView?.append(userInput.text)
+                textView?.append("\n")
+//                userInput.text.clear() // this is the same as next line
+                userInput.setText("")   // both are ways of clearing the text 
+
+            }
+
+})
+
+    }
+}
